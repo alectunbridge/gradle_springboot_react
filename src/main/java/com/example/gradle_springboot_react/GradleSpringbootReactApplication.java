a449a802776f2339f323dfeedf2b87421dcb5fe4
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2020-03-11 15:48:06 +0000

    Initial commit

---
+++ src/main/java/com/example/gradle_springboot_react/GradleSpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle_springboot_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringbootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringbootReactApplication.class, args);
+	}
+
+}
